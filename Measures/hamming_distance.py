75e831ddf9fb9f1f276a188a3efefe2ad28cce99
soschuetze <ss6@wellesley.edu>
2024-03-07 15:54:40 +0000

    Change function to calculate for individual pair of graphs

--- Measures/hamming_distance.py
+++ Measures/hamming_distance.py
@@ -1,25 +1,24 @@
-def hamming_distance(adj_arr):
+import networkx as nx
+
+def hamming_distance(G1, G2):
   '''
   Calculates hamming distance between each pair of neighboring graphs at time t and t+1
   ## Parameters
-  adj_arr: array of adjacency matrices at each time t
+  G1: graph at time t
+  G2: graph at time t+1
 
   ## Return 
-  Array of hamming distances
+  Hamming distance value
   '''
-  
-  h_distances = []
-  for graph in range(len(adj_arr)-1):
-    t_1 = adj_arr[graph]
-    t_2 = adj_arr[graph+1]
 
-    N = len(t_1)
-  
-    sum = 0
-    for row_1, row_2 in zip(t_1, t_2):
+  t_1 = nx.to_numpy_array(G1)
+  t_2 = nx.to_numpy_array(G2)
+
+  N = len(t_1)
+
+  sum = 0
+  for row_1, row_2 in zip(t_1, t_2):
       for value_1, value_2 in zip(row_1, row_2):
-        sum += abs(value_1-value_2)/(N*(N-1))
-
-    h_distances.append(sum)
-
-  return h_distances
+          sum += abs(value_1-value_2)/(N*(N-1))
+  
+  return sum
